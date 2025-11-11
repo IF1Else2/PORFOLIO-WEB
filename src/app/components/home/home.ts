adeb4913b9e3f3dc5358c8b5bb26c9a46ff559e8
IF1Else2 <titealuche@gmail.com>
2025-11-11 16:34:54 +0100

    Fix rutas de imágenes para GitHub Pages

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,            
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class Home { }
+export class Home implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const elements = document.querySelectorAll<HTMLElement>('.scroll-anim');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const el = entry.target as HTMLElement;
+          el.classList.add('visible');
+          observer.unobserve(el); // si quieres que solo se anime una vez
+        }
+      });
+    });
+
+    elements.forEach((el) => observer.observe(el));
+  }
+}
